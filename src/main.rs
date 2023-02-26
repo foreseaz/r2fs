a78aabc5dba83e06f71cbea0853c9a869f2f5418
chenxizh <foreseazhang@gmail.com>
2023-02-26 10:41:39 -0700

    feat: add reqwest for API call

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
 use fuser::{Filesystem, MountOption};
+use serde_json::Value;
+use std::error::Error;
 use std::{env, process};
 
 struct R2FileSystem;
 
 impl Filesystem for R2FileSystem {
     // Implement the FUSE operations here
+}
+
+fn get_r2(client: &reqwest::blocking::Client) -> Result<Vec<String>, Box<dyn Error>> {
+    let response = client
+        .get("https://api.cloudflare.com/client/v4/accounts/:account_id/r2/buckets")
+        .header("Authorization", "Bearer :token")
+        .send()?;
+
+    let json: Value = response.json()?;
+    let namespaces = json["result"]
+        .as_array()
+        .ok_or("Unexpected JSON format")?;
+
+    let mut buckets = Vec::new();
+    for namespace in namespaces {
+        let name = namespace["title"].as_str().unwrap_or_default();
+        if !name.is_empty() {
+            buckets.push(name.to_owned());
+        }
+    }
+
+    Ok(buckets)
 }
 
 fn main() {
@@ -18,6 +42,11 @@ fn main() {
             process::exit(1);
         }
     };
+
+    let client = reqwest::blocking::Client::new();
+    let res = get_r2(&client)?;
+    // Print the list of buckets to confirm we received them
+    println!("Buckets: {:?}", res);
 
     // Set up the mount options
     let mut mount_options = Vec::new();
