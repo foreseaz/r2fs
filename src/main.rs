01d6049b89676990e81ba5de3b023621b5620727
chenxizh <foreseazhang@gmail.com>
2023-03-18 12:26:32 -0700

    feat: add .env and listBucket

--- src/main.rs
+++ src/main.rs
@@ -2,37 +2,54 @@ use fuser::{Filesystem, MountOption};
 use serde_json::Value;
 use std::error::Error;
 use std::{env, process};
+use dotenv::dotenv;
 
-struct R2FileSystem;
+struct R2FS {
+    cf_account_id: String,
+    cf_api_token: String,
+}
 
-impl Filesystem for R2FileSystem {
+impl Filesystem for R2FS {
     // Implement the FUSE operations here
 }
 
-fn get_r2(client: &reqwest::blocking::Client) -> Result<Vec<String>, Box<dyn Error>> {
+fn get_r2(client: &reqwest::blocking::Client, fs: &R2FS) -> Result<Vec<String>, Box<dyn Error>> {
+    let endpoint = format!("https://api.cloudflare.com/client/v4/accounts/{}/r2/buckets", fs.cf_account_id);
     let response = client
-        .get("https://api.cloudflare.com/client/v4/accounts/:account_id/r2/buckets")
-        .header("Authorization", "Bearer :token")
+        .get(endpoint)
+        .header("Authorization", format!("Bearer {}", fs.cf_api_token))
         .send()?;
 
     let json: Value = response.json()?;
-    let namespaces = json["result"]
+    let namespaces = json["result"]["buckets"]
         .as_array()
         .ok_or("Unexpected JSON format")?;
 
     let mut buckets = Vec::new();
     for namespace in namespaces {
-        let name = namespace["title"].as_str().unwrap_or_default();
+        let name = namespace["name"].as_str().unwrap_or_default();
         if !name.is_empty() {
             buckets.push(name.to_owned());
         }
     }
 
+    println!("[INFO] Received buckets: {:#?}", buckets);
+
     Ok(buckets)
 }
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     println!("Starting the R2FS...");
+
+    dotenv().ok(); // load the .env file
+    let cf_account_id = env::var("ACCOUNT_ID").unwrap();
+    let cf_api_token = env::var("CLOUDFLARE_API_TOKEN").unwrap();
+
+    let fs = R2FS {
+        cf_account_id: cf_account_id.clone(),
+        cf_api_token: cf_api_token.clone(),
+    };
+    println!("FS: {}", fs.cf_account_id);
 
     // Get the mountpoint argument
     let mountpoint = match env::args_os().nth(1) {
@@ -42,11 +59,10 @@ fn main() {
             process::exit(1);
         }
     };
+    println!("Mount at: {:?}", mountpoint);
 
     let client = reqwest::blocking::Client::new();
-    let res = get_r2(&client)?;
-    // Print the list of buckets to confirm we received them
-    println!("Buckets: {:?}", res);
+    let res = get_r2(&client, &fs)?;
 
     // Set up the mount options
     let mut mount_options = Vec::new();
@@ -54,5 +70,7 @@ fn main() {
     mount_options.push(MountOption::FSName("r2".to_string()));
 
     // Mount the file system
-    fuser::mount2(R2FileSystem, &mountpoint, &mount_options).unwrap();
+    // fuser::mount2(R2FS, &mountpoint, &mount_options).unwrap();
+
+    Ok(())
 }
